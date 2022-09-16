85251b92588b8088242278840ecd8b9cd4c86d9a
oludammydy <oludammydy@gmail.com>
2022-09-16 06:28:53 +0100

    Added new lessons learnt

--- app.jsx
+++ app.jsx
@@ -322,3 +322,41 @@ Using curly braces, set the <h1></h1>‘s inner text equal to theBestString. */}
         const theBestString = 'tralalalala i am da best';
 
         ReactDOM.render(<h1>{theBestString}</h1>, document.getElementById('app'));
+
+
+        {/* Variable Attributes in JSX - When writing JSX, it’s common to use variables to set attributes.*/}
+        // Use a variable to set the `height` and `width` attributes:
+
+        const sideLength = "200px";
+
+        const panda = (
+        <img
+          src="images/panda.jpg"
+          alt="panda"
+          height={sideLength}
+          width={sideLength} />
+        );
+
+        const pics = {
+          panda: "http://bit.ly/1Tqltv5",
+        owl: "http://bit.ly/1XGtkM3",
+        owlCat: "http://bit.ly/1Upbczi"
+};
+
+        const panda = (
+        <img
+          src={pics.panda}
+          alt="Lazy Panda" />
+        );
+
+        const owl = (
+        <img
+          src={pics.owl}
+          alt="Unimpressed Owl" />
+        );
+
+        const owlCat = (
+        <img
+          src={pics.owlCat}
+          alt="Ghastly Abomination" />
+        ); 
